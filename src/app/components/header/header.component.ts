cf45ddd619453495b48e7d439b6a37bbf9fd8407
EzequielDalzotto <dalzottoezequiel@gmail.com>
2023-04-24 13:27:56 -0300

    toggle ui for adding task

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/service/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,14 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent {
   title:string = 'Todo-List';
+  showAddTask:boolean = false;
+  subscription?: Subscription;
 
-  constructor(){ }
+  constructor(
+    private uiService:UiService
+    
+  ){ 
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
 
   ngOnInit():void{
   }
 
   toggleAddtask(){
-    console.log("toggleAddtask")
+    this.uiService.toggleAddTask();
   }
 
 }
