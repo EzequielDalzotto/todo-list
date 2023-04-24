3b6b995a97240e0794c0d8a989361fdf26908beb
EzequielDalzotto <dalzottoezequiel@gmail.com>
2023-04-24 02:19:16 -0300

    componente de lista de items

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-tasks-item',
@@ -9,5 +10,6 @@ import { TASKS } from 'src/app/mock-task';
 })
 export class TasksItemComponent {
   @Input() task:Task = TASKS[0];
+  faTimes = faTimes;
 
 }
